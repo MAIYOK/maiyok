2e788b83e70a604dea037eba86d6790db83a6b28
MAIYOK <maiyokevnn@gmail.com>
2025-05-23 17:22:45 +0300

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Nuru Ya Maendeleo Loan Site</h1>
-      <p>This is the homepage. Use the navigation to login or view dashboard.</p>
-    </div>
-  );
-}
