7aad9f38a7410faca531e6d274c51673b8a3f9f8
MAIYOK <maiyokevnn@gmail.com>
2025-05-23 09:08:10 +0300

    Create Add index.js

---
+++ Add index.js
@@ -0,0 +1,8 @@
+pages/index.jsexport default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Nuru Ya Maendeleo Loan Site</h1>
+      <p>This is the homepage. Use the navigation to login or view dashboard.</p>
+    </div>
+  );
+}
