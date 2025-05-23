b84d5d3f3a837b3567443fa94fe7a21b8714a017
MAIYOK <maiyokevnn@gmail.com>
2025-05-23 09:57:05 +0300

    Delete pages directory

--- pages/dashboard.js
+++
@@ -1,8 +0,0 @@
-export default function Dashboard() {
-  return (
-    <div style={{ padding: 20 }}>
-      <h1>Admin Dashboard</h1>
-      <p>Welcome to Nuru Ya Maendeleo Dashboard</p>
-    </div>
-  );
-}
